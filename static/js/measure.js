558d66dac4d11205f1a66107da8777305496cb56
CrashedBboy <crashedbboy@gmail.com>
2019-06-26 15:59:47 +0800

    add marks and labels for measurements

--- static/js/measure.js
+++ static/js/measure.js
@@ -1,14 +1,27 @@
 const DEBUG = true;
 
+// tileset path
 var TILESET = 'static/tile/road/tile_0_0_0_tex/tileset.json';
+
+// 3DTileset entity
 var tileset;
+
+// Cesium viewer
 var viewer;
+
+// Cesium event handler
 var handler;
 
-var startEntity = null;
+// Billboard entity at starting position of line
+var startBillboard = null;
 var startPosition = null;
-var endEntity = null;
-var endPosition = null;
+
+// Polyline entity which is drawed during moving mouse
+var movingLine = null;
+var movingPosition = null;
+
+// Polyline of completed measurement
+var completedLines = [];
 
 $(document).ready(function () {
 
@@ -16,72 +29,129 @@ $(document).ready(function () {
         selectionIndicator: false
     });
     
-    viewer.scene.globe.baseColor = Cesium.Color.BLACK;
-
     load3DTileset(TILESET);
 
     handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas);
 
     handler.setInputAction((click) => {
 
+        // get position of 2D window (X,Y)
         let windowPosition = click.position;
-
-        // console.log('window position', windowPosition);
 
         if (windowPosition) {
 
+            // using 2D window's position to get 3D world position
             let worldPosition = viewer.scene.pickPosition(windowPosition);
 
-            // console.log('world position', worldPosition);
+            // start position of a measurement line hasn't been picked
+            if (startBillboard == null) {
 
-            if (startEntity == null && endEntity == null) {
+                $("body").css("cursor", "crosshair");
 
-                startEntity = viewer.entities.add({
+                startBillboard = viewer.entities.add({
                     position: worldPosition,
-                    ellipsoid : {
-                        radii : new Cesium.Cartesian3(0.5, 0.5, 0.5),
-                        material : Cesium.Color.RED.withAlpha(0.8)
+                    billboard: {
+                        image : 'static/image/pin_red.png',
+                        pixelOffset : new Cesium.Cartesian2(0, -16),
+                        scale : 1.0,
+                        width : 24,
+                        height : 32,
+                        disableDepthTestDistance: Number.POSITIVE_INFINITY
                     }
                 });
 
                 startPosition = worldPosition;
             
-            } else if (startEntity != null && endEntity == null) {
+            // start point has been picked, but end point hasn't been picked
+            } else if (startBillboard != null) {
 
-                endEntity = viewer.entities.add({
+                $("body").css("cursor", "auto");
+
+                viewer.entities.add({
                     position: worldPosition,
-                    ellipsoid : {
-                        radii : new Cesium.Cartesian3(0.5, 0.5, 0.5),
-                        material : Cesium.Color.BLUE.withAlpha(0.8)
+                    billboard: {
+                        image : 'static/image/pin_red.png',
+                        pixelOffset : new Cesium.Cartesian2(0, -16),
+                        scale : 1.0,
+                        width : 24,
+                        height : 32,
+                        disableDepthTestDistance: Number.POSITIVE_INFINITY
                     }
                 });
 
-                endPosition = worldPosition;
+                startBillboard = null;
 
-                console.log('distance', Cesium.Cartesian3.distance(startPosition, endPosition));
+                completedLines.push(movingLine);
+                movingLine = null;
 
-            } else if (startEntity != null && endEntity != null) {
+                let distance = Cesium.Cartesian3.distance(startPosition, worldPosition)
 
-                viewer.entities.remove(startEntity);
-                viewer.entities.remove(endEntity);
+                // draw distance label
+                let center = new Cesium.Cartesian3(
+                    (worldPosition.x + startPosition.x)/2,
+                    (worldPosition.y + startPosition.y)/2,
+                    (worldPosition.z + startPosition.z)/2
+                );
 
-                startEntity = viewer.entities.add({
-                    position: worldPosition,
-                    ellipsoid : {
-                        radii : new Cesium.Cartesian3(0.5, 0.5, 0.5),
-                        material : Cesium.Color.RED.withAlpha(0.8)
+                viewer.entities.add({
+                    position : center,
+                    label : {
+                        text : Math.floor(distance * 100) / 100 + ' m',
+                        font: '16px sans-serif',
+                        disableDepthTestDistance: Number.POSITIVE_INFINITY,
                     }
                 });
 
-                startPosition = worldPosition;
-
-                endEntity = null;
-                endPosition = null;
+                $('#distance-tag').css('display', 'none');
             }
-            
         }
 
     }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+
+    handler.setInputAction((move) => {
+
+        if (startBillboard != null) {
+
+            let windowPosition = move.endPosition;
+
+            if (windowPosition) {
+
+                let worldPosition = viewer.scene.pickPosition(windowPosition);
+
+                movingPosition = worldPosition;
+
+                if (movingLine == null) {
+
+                    movingLine = viewer.entities.add({
+                        polyline : {
+                            positions : [startPosition, worldPosition],
+                            width : 2,
+                            material : new Cesium.PolylineDashMaterialProperty({
+                                color: Cesium.Color.WHITE
+                            }),
+                            depthFailMaterial: new Cesium.PolylineDashMaterialProperty({
+                                color: Cesium.Color.RED.withAlpha(0.5)
+                            })
+                        }
+                    });
+
+                } else {
+                    movingLine.polyline.positions = [startPosition, worldPosition];
+                }
+
+                let distance = Cesium.Cartesian3.distance(startPosition, worldPosition);
+                $('#distance-tag').css('display', 'inline-block').text(Math.floor(distance*100) / 100 + ' m');
+            }
+        }
+
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+});
+
+$(document).on('mousemove', function(e){
+    $('#distance-tag').css({
+       left:  e.pageX + 10,
+       top:   e.pageY + 10
+    });
 });
 
 function load3DTileset(url, debug = false) {
